70a873ddade9c9423513b9737339a0a120c43741
Henrik Östman <trycoon@gmail.com>
2016-09-25 07:47:31 +0200

    More fixes, should be working now more or less

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sensor-service
-Simple Python daemon for reading sensors and logging to a InfluxDB
+Simple Python daemon for reading 1-wire sensors and logging to a InfluxDB.
+Graphs can later be created with Grafana.
--- sensor-service.py
+++ sensor-service.py
@@ -6,85 +6,102 @@ import pyownet
 from influxdb import InfluxDBClient
 from daemon import Daemon
 
-# change this to the pin used to monitor the rain sensor
-rain_sensor_pin = 23
-host='localhost'
-port = 8086
-user = "root"
-password = "root"
-db_name = "sensors"
+class MyDaemon(Daemon):
+	def run(self):
 
-print 'Starting sensor daemon.'
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(rain_sensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		# change this to the pin used to monitor the rain sensor
+		rain_sensor_pin = 23
+		# database engine host
+		host='localhost'
+		# database engine port
+		port = 8086
+		# database engine user
+		user = "root"
+		# database engine password
+		password = "root"
+		# database to save all our logging to
+		db_name = "sensors"
 
-def rain_trigger_callback(channel):
-	print 'The Rain Keeps Pouring Down. ' + time.strftime('%a, %d %b %Y %H:%M:%S %Z(%z)')
+		print 'Starting sensor daemon.'
+		# setup raspberry pi pins
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(rain_sensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		# setup influx database engine connection and sensor database 
+		db = InfluxDBClient(host, port, user, password, db_name)
 
-# setup influx database engine connection and sensor database 
-db = InfluxDBClient(host, port, user, password, db_name)
+		# function to be called when it's raining 
+		def rain_trigger_callback(channel):
+			print 'The Rain Keeps Pouring Down. ' + time.strftime('%a, %d %b %Y %H:%M:%S %Z(%z)')
+			# setup values to save to the database. database will add timestamp automatically
+			json_body = [
+    			{
+        			"measurement": "rain",
+        			"tags": {},
+        			"fields": {
+            				"value": 1.0
+        			}
+    			}]
+			# save to database
+			db.write_points(json_body)
 
-# register callback for rising-edge interrupt on rain sensor pin, and a debounce time of 500 ms.
-GPIO.add_event_detect(rain_sensor_pin, GPIO.RISING, callback=rain_trigger_callback, bouncetime=500)  
+		# register callback for rising-edge interrupt on rain sensor pin, and add a debounce time of 1000 ms.
+		GPIO.add_event_detect(rain_sensor_pin, GPIO.RISING, callback=rain_trigger_callback, bouncetime=1000)  
 
-try:
-	# setup owserver connection
-	owproxy = pyownet.protocol.proxy(host='localhost', port=4304, flags=0, persistent=False, verbose=False)
-	# get list of all available 1-wire sensors, but only the sensors that starts with family name = 10 (DS1820)
-	owdevices = [d for d in owproxy.dir() if '10.' in d]
-	print 'Found owdevices: ' + str(owdevices)
+		try:
+			# setup owserver(1-wire) connection
+			owproxy = pyownet.protocol.proxy(host='localhost', port=4304, flags=0, persistent=False, verbose=False)
+			# get list of all available 1-wire sensors, but only the sensors that starts with family name = 10 (DS1820)
+			owdevices = [d for d in owproxy.dir() if '10.' in d]
+			print 'Found owdevices: ' + str(owdevices)
 
-	while True:
-		#owproxy.read()
-		#db.write_points_with_precision([{"points":[[1400000000000,1,1,1]], "name":"foo", "columns":["time", "a","b","c"]}], time_precision='u')
-		# sample temperature every 600 seconds(10 minutes)
-		time.sleep(600)
+			# loop until program exists
+			while True:
+				# loop found 1-wire devices
+				for id in owdevices:
+					# remove '/' in 1-wire device names, we don't need them
+					id = id.replace('/', '')
+					# read temperature from one sensor, convert reading to a float-value
+					temp = float(owproxy.read('/{0}/temperature'.format(id)))
+					print 'Read temperature: {0} from device: {1}'.format(temp, id)
+					# setup values to save to the database. database will add timestamp automatically
+        				json_body = [
+        				{
+                				"measurement": "temperature",
+                				"tags": { 
+							"sensor": id
+						},
+                				"fields": {
+                        				"value": temp
+                				}
+        				}]
+					# save to database
+        				db.write_points(json_body)
+
+				# wait 600 seconds(10 minutes) before we read sensors again
+				time.sleep(600)
 	
-except pyownet.protocol.Error as err:
-        log = open('/home/pi/christer-service/error.log', 'w')
-        log.write('Something went wrong: {}'.format(err))
-        log.close()
-	GPIO.cleanup()
-except KeyboardInterrupt:  
-	GPIO.cleanup()
+		except pyownet.protocol.Error as err:
+        		log = open('/var/log/sensor-service/error.log', 'w')
+        		log.write('Something went wrong: {}'.format(err))
+        		log.close()
+			GPIO.cleanup()
+		except KeyboardInterrupt:  
+			GPIO.cleanup()
 
-
-#class MyDaemon(Daemon):
-#	def run(self):
-#	        # Setup rain-sensor input.
-#		print "Starting sensor service."	
-#		GPIO.setmode(GPIO.BCM)
-#		GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#
-#		try:
-#			# setup owserver connection
-#			owproxy = pyownet.protocol.proxy(host='localhost', port=4304, flags=0, persistent=False, verbose=False)
-#		        print "Got connection to owserver."	
-#			devices = owproxy.dir()
-#	               
-#			while True:
- #       	        	GPIO.wait_for_edge(23, GPIO.FALLING)
-#				time.sleep(0.5)
-#		except pyownet.protocol.Error as err:
-#			log = open('/home/pi/christer-service/error.log', 'w')
-#			log.write('Something went wrong: {}'.format(err))
-#			log.close()
-#		GPIO.cleanup()
-#
-#if __name__ == "__main__":
-#	daemon = MyDaemon('/tmp/christer-service.pid')
-#	if len(sys.argv) == 2:
-#		if 'start' == sys.argv[1]:
-#			daemon.start()
-#		elif 'stop' == sys.argv[1]:
-#			daemon.stop()
-#		elif 'restart' == sys.argv[1]:
-#			daemon.restart()
-#		else:
-#			print "Unknown command"
-#			sys.exit(2)
-#		sys.exit(0)
-#	else:
-#		print "usage: %s start|stop|restart" % sys.argv[0]
-#		sys.exit(2)
+if __name__ == "__main__":
+	daemon = MyDaemon('/var/run/sensor-service.pid')
+	if len(sys.argv) == 2:
+		if 'start' == sys.argv[1]:
+			daemon.start()
+		elif 'stop' == sys.argv[1]:
+			daemon.stop()
+		elif 'restart' == sys.argv[1]:
+			daemon.restart()
+		else:
+			print "Unknown command"
+			sys.exit(2)
+		sys.exit(0)
+	else:
+		print "usage: %s start|stop|restart" % sys.argv[0]
+		sys.exit(2)
 
